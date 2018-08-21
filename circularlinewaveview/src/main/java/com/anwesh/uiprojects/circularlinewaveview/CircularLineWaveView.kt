68376021db6fbcbf8d789e7a42126d1cbb50b5fd
Anwesh43 <anweshthecool0@gmail.com>
2018-08-22 02:42:13 +0530

    adding CircularLineWave class

--- circularlinewaveview/src/main/java/com/anwesh/uiprojects/circularlinewaveview/CircularLineWaveView.kt
+++ circularlinewaveview/src/main/java/com/anwesh/uiprojects/circularlinewaveview/CircularLineWaveView.kt
@@ -144,6 +144,28 @@ class CircularLineWaveView (ctx : Context) : View(ctx) {
             cb()
             return this
         }
+    }
 
+    data class CircularLineWave(var i : Int) {
+
+        private var curr : CLWNode = CLWNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
     }
 }
