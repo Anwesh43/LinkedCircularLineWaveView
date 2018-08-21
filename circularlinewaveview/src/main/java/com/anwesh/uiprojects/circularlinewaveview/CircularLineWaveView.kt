d94fb15e938ea2717633cb84b20a729997e3679e
Anwesh43 <anweshthecool0@gmail.com>
2018-08-22 02:07:43 +0530

    importing libraries and defining nodes

--- circularlinewaveview/src/main/java/com/anwesh/uiprojects/circularlinewaveview/CircularLineWaveView.kt
+++ circularlinewaveview/src/main/java/com/anwesh/uiprojects/circularlinewaveview/CircularLineWaveView.kt
@@ -3,3 +3,12 @@ package com.anwesh.uiprojects.circularlinewaveview
 /**
  * Created by anweshmishra on 22/08/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Path
+
+val nodes : Int = 5 
