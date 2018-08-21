9fdcec5d937e08d8280018c1ca2a711ec786c3a2
Anwesh43 <anweshthecool0@gmail.com>
2018-08-22 02:32:24 +0530

    adding drawCLWNode canvas extension method

--- circularlinewaveview/src/main/java/com/anwesh/uiprojects/circularlinewaveview/CircularLineWaveView.kt
+++ circularlinewaveview/src/main/java/com/anwesh/uiprojects/circularlinewaveview/CircularLineWaveView.kt
@@ -9,9 +9,34 @@ import android.view.MotionEvent
 import android.content.Context
 import android.graphics.Paint
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Path
 
 val nodes : Int = 5
+
+fun Canvas.drawCLWNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val index : Int = (i) % 2
+    val index1 : Int = (i / 2) % 2
+    val r : Float = gap / 2
+    paint.color = Color.parseColor("#1976D2")
+    save()
+    translate(w / 2, gap / 2 + gap * i)
+    val path : Path = Path()
+    for (j in 270..(270 + (180 * scale).toInt())) {
+        val x : Float = (r) * (1 - 2 * index1) * index * Math.cos(j *   Math.PI/180).toFloat()
+        val y : Float = (r) * (1 - 2 * index1) * index * Math.sin(j *   Math.PI/180).toFloat()
+        if (i == 270) {
+            path.moveTo(x, y)
+        } else {
+            path.lineTo(x, y)
+        }
+    }
+    drawPath(path, paint)
+    restore()
+}
 
 class CircularLineWaveView (ctx : Context) : View(ctx) {
 
